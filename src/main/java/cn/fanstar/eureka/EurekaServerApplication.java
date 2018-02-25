0a09b98a9cfa21f725ddec8c67bdc0b267c5ffc9
fanjun <fanjun301@qq.com>
2018-02-25 10:55:39 +0800

    add some config

--- src/main/java/cn/fanstar/eureka/EurekaServerApplication.java
+++ src/main/java/cn/fanstar/eureka/EurekaServerApplication.java
@@ -9,6 +9,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		try {
+			SpringApplication.run(EurekaServerApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
